d9aeb32d9f1401e5c2f3677f261987bf844686cf
muskansharmavaid06 <134607549+muskansharmavaid06@users.noreply.github.com>
2026-08-09 03:32:13 +0530

    Update app.py

--- Predictive_Modeling_for_Course_Demand_and_Revenue_Forecasting_on_EduPro/app.py
+++ Predictive_Modeling_for_Course_Demand_and_Revenue_Forecasting_on_EduPro/app.py
@@ -24,7 +24,7 @@ def load_excel_data(file_path_or_buffer):
     """Reads Excel file containing Users, Teachers, Courses, and Transactions sheets."""
    
     file_path = Path('Predictive_Modeling_for_Course_Demand_and_Revenue_Forecasting_on_EduPro') / 'read_edu.xlsx';
-     xls = pd.ExcelFile(file_path)
+    xls = pd.ExcelFile(file_path)
     # Raw Sheet Loading
     raw_users_df = pd.read_excel(xls, sheet_name='Users')
     raw_teachers_df = pd.read_excel(xls, sheet_name='Teachers')
